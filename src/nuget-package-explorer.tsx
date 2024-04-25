bf04e982db5ce6113629a333e1f580051f700012
gaoyang <xz@lgysh.cn>
2024-04-25 11:21:18 +0800

    get more possible icons.

--- src/nuget-package-explorer.tsx
+++ src/nuget-package-explorer.tsx
@@ -14,58 +14,66 @@ export default function Command() {
 
   return (
     <List isShowingDetail searchBarPlaceholder="Search Nuget package">
-      {nugetPackages.map((item, index) => (
-        <List.Item
-          key={index}
-          title={item.name}
-          icon={{ source: item.icon || "icon.png", fallback: "icon.png" }}
-          actions={
-            <ActionPanel>
-              <Action title="Open Folder" onAction={() => open(item.path)} />
-            </ActionPanel>
-          }
-          detail={
-            <List.Item.Detail
-              markdown={`
-![icon](${item.icon || "icon.png"}?raycast-width=100&raycast-height=100)   
+      {nugetPackages.map((item, index) => {
+        return (
+          <List.Item
+            key={index}
+            id={item.id}
+            title={item.name}
+            subtitle={item.versions[0]}
+            icon={{
+              source: item.icon || "icon.png",
+              fallback: "icon.png",
+            }}
+            actions={
+              <ActionPanel>
+                <Action title="Open Folder" onAction={() => open(item.path)} />
+              </ActionPanel>
+            }
+            detail={
+              <List.Item.Detail
+                markdown={`
+![asd![icon](icon.png?raycast-width=100&raycast-height=100)](${item.icon || "icon.png"}?raycast-width=100&raycast-height=100 )   
 ${item.description || ""}
 `}
-              metadata={
-                <List.Item.Detail.Metadata>
-                  {item.authors && <List.Item.Detail.Metadata.Label title="Authors" text={item.authors} />}
-                  {item.owners && <List.Item.Detail.Metadata.Label title="Owners" text={item.owners} />}
+                metadata={
+                  <List.Item.Detail.Metadata>
+                    {item.authors && <List.Item.Detail.Metadata.Label title="Authors" text={item.authors} />}
+                    {item.owners && <List.Item.Detail.Metadata.Label title="Owners" text={item.owners} />}
 
-                  {(item.projectUrl || item.licenseUrl) && (
-                    <List.Item.Detail.Metadata.TagList title="Links">
-                      {item.projectUrl && (
-                        <List.Item.Detail.Metadata.TagList.Item
-                          text="Project ↗"
-                          color="PrimaryText"
-                          onAction={() => open(item.projectUrl!)}
-                        />
-                      )}
-                      {item.repositoryUrl && (
-                        <List.Item.Detail.Metadata.TagList.Item
-                          text="Repository ↗"
-                          onAction={() => open(item.repositoryUrl!)}
-                        />
-                      )}
-                      {item.licenseUrl && (
-                        <List.Item.Detail.Metadata.TagList.Item
-                          text="License ↗"
-                          onAction={() => open(item.licenseUrl!)}
-                        />
-                      )}
-                    </List.Item.Detail.Metadata.TagList>
-                  )}
+                    {(item.projectUrl || item.repositoryUrl || item.licenseUrl) && (
+                      <List.Item.Detail.Metadata.TagList title="Links">
+                        {item.projectUrl && (
+                          <List.Item.Detail.Metadata.TagList.Item
+                            text="Project ↗"
+                            color="PrimaryText"
+                            onAction={() => open(item.projectUrl!)}
+                          />
+                        )}
+                        {item.repositoryUrl && (
+                          <List.Item.Detail.Metadata.TagList.Item
+                            text="Repository ↗"
+                            onAction={() => open(item.repositoryUrl!)}
+                          />
+                        )}
+                        {item.licenseUrl && (
+                          <List.Item.Detail.Metadata.TagList.Item
+                            text="License ↗"
+                            onAction={() => open(item.licenseUrl!)}
+                          />
+                        )}
+                      </List.Item.Detail.Metadata.TagList>
+                    )}
 
-                  {item.tags && <List.Item.Detail.Metadata.Label title="Tags" text={item.tags} />}
-                </List.Item.Detail.Metadata>
-              }
-            />
-          }
-        ></List.Item>
-      ))}
+                    <List.Item.Detail.Metadata.Label title="Versions" text={item.versions.join("、")} />
+                    {item.tags && <List.Item.Detail.Metadata.Label title="Tags" text={item.tags} />}
+                  </List.Item.Detail.Metadata>
+                }
+              />
+            }
+          ></List.Item>
+        );
+      })}
     </List>
   );
 }
